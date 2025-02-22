e52be100bba56830c77a6b26efb0d5d7e6fd3967
minyoung <79049046+zhal7779@users.noreply.github.com>
2025-02-23 00:32:44 +0900

    Time: 0 ms (100%), Space: 54.6 MB (5.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,18 +3,16 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    let answer = [-1, -1];
-
-    for(let i =0; i<nums.length; i++) {
-
-      for(let j = i+1; j<nums.length; j++) {
-        if(nums[i] + nums[j] === target) {
-            answer = [i, j]
+var twoSum = function (nums, target) {
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+    
+        if (map.has(complement)) {
+            return [map.get(complement), i];
         }
-
-      }
+        map.set(nums[i], i);
     }
 
-    return answer;
+    return [];
 };
