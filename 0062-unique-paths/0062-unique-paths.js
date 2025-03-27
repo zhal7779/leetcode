89bcd6e60ad5ed9c5fac6930069c80818efcb267
minyoung <79049046+zhal7779@users.noreply.github.com>
2025-03-27 23:35:30 +0900

    Time: 0 ms (100%), Space: 53.5 MB (45.37%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+    let dp = Array(m).fill().map(() => Array(n).fill(1));
+
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+        }
+    }
+    
+    return dp[m - 1][n - 1];
+};
